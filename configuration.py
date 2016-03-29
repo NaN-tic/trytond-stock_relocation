b7387aba7c1b94702760b7f43df81bad30698a55
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:47 +0200

    Add python 3 support

--- configuration.py
+++ configuration.py
@@ -5,10 +5,10 @@ from trytond.model import fields
 from trytond.pool import PoolMeta
 
 __all__ = ['Configuration']
-__metaclass__ = PoolMeta
 
 
 class Configuration:
+    __metaclass__ = PoolMeta
     __name__ = 'stock.configuration'
     to_relocation = fields.Property(fields.Many2One("stock.location",
         "To ReLocation", domain=[('type', 'not in', ('warehouse', 'view'))]))
