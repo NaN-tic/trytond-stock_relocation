8c143703f1be70671b557a5f277594fe4c753621
Raimon Esteve <raimon@nan-tic.com>
2017-01-13 14:49:10 +0100

    Stock Relocation scenario

--- tests/test_stock_relocation.py
+++ tests/test_stock_relocation.py
@@ -1,9 +1,11 @@
-# This file is part of the stock_relocation module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of the sale_line_standalone module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
-import trytond.tests.test_tryton
+import doctest
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import suite as test_suite
 
 
 class StockRelocationTestCase(ModuleTestCase):
@@ -12,7 +14,10 @@ class StockRelocationTestCase(ModuleTestCase):
 
 
 def suite():
-    suite = trytond.tests.test_tryton.suite()
+    suite = test_suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        StockRelocationTestCase))
+            StockRelocationTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_stock_relocation.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
